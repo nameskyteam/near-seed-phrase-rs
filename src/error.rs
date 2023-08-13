d6184f2b39f183a2bd65d2279a4b1f63edcfab89
sakura <zjchen1234@foxmail.com>
2023-08-13 20:56:39 +0800

    error

--- src/error.rs
+++ src/error.rs
@@ -4,16 +4,16 @@ use std::fmt::Debug;
 #[non_exhaustive]
 pub enum Error {
     #[error("{0}")]
-    Slip10(slip10::Error),
+    Slip10Error(slip10::Error),
 
     #[error("{0}")]
-    Bip39(#[from] bip39::Error),
+    Bip39Error(#[from] bip39::Error),
 
     #[error("{0}")]
-    Signature(#[from] ed25519_dalek::SignatureError),
+    SignatureError(#[from] ed25519_dalek::SignatureError),
 
     #[error("{0}")]
-    Base58Decode(#[from] bs58::decode::Error),
+    Base58DecodeError(#[from] bs58::decode::Error),
 
     #[error("invalid secret key length")]
     InvalidSecretKeyLen,
@@ -24,6 +24,6 @@ pub enum Error {
 
 impl From<slip10::Error> for Error {
     fn from(e: slip10::Error) -> Self {
-        Self::Slip10(e)
+        Self::Slip10Error(e)
     }
 }
