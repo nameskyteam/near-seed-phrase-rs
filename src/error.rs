aab3c0b9ba2b99f5a3899b62879907b6355fc287
coffee <95295094+hanakannzashi@users.noreply.github.com>
2023-11-24 18:48:50 +0800

    fix: error (#3)

--- src/error.rs
+++ src/error.rs
@@ -1,22 +1,23 @@
 #[derive(std::fmt::Debug, thiserror::Error)]
 #[non_exhaustive]
 pub enum Error {
+    #[error("invalid byte length")]
+    InvalidByteLength,
+
     #[error("{0}")]
     Slip10Error(slip10::Error),
 
-    #[error("{0}")]
+    #[error(transparent)]
     Bip39Error(#[from] bip39::Error),
 
-    #[error("{0}")]
+    #[error(transparent)]
     Ed25519SignatureError(#[from] ed25519_dalek::SignatureError),
 
-    #[error("{0}")]
+    #[error(transparent)]
     Base58DecodeError(#[from] bs58::decode::Error),
-
-    #[error("invalid byte length")]
-    InvalidByteLength,
 }
 
+// `slip10::Error` doesn't satisfy `std::error::Error`, so manually implement `From` trait
 impl From<slip10::Error> for Error {
     fn from(e: slip10::Error) -> Self {
         Self::Slip10Error(e)
