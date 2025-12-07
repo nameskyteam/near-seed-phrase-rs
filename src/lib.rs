52107c740169568401b678ff86c549e5f6a505df
sakura <zjchen1234@foxmail.com>
2025-12-07 15:34:22 +0800

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -33,29 +33,29 @@ mod test {
     const MNEMONIC: &str =
         "fortune conduct light unusual gloom process wrap spare season exact anchor devote";
 
-    const ENCODED_PRIVATE_KEY: &str =
+    const PRIVATE_KEY: &str =
         "ed25519:G94YBVktAVUFZWvYBtYmfpvVMNCtSf2x73bMfTCM9CfzyrUyN5X6VpTqr8QTCHYBTdUfzufDsTy3cR9CfNf74Bv";
 
-    const ENCODED_PUBLIC_KEY: &str = "ed25519:2PQENDq3KABdr7cw1TH5B4AdXLqcyNXTTpWbdZh7k828";
+    const PUBLIC_KEY: &str = "ed25519:2PQENDq3KABdr7cw1TH5B4AdXLqcyNXTTpWbdZh7k828";
 
     #[test]
     fn test_derive_key() {
         let mnemonic = MNEMONIC.parse::<NearMnemonic>().unwrap();
         let private_key = derive_key(&mnemonic, "", &NearDerivationPath::default()).unwrap();
 
-        assert_eq!(private_key.to_string(), ENCODED_PRIVATE_KEY);
-        assert_eq!(private_key.get_public_key().to_string(), ENCODED_PUBLIC_KEY);
+        assert_eq!(private_key.to_string(), PRIVATE_KEY);
+        assert_eq!(private_key.get_public_key().to_string(), PUBLIC_KEY);
     }
 
     #[test]
     fn test_parse_key() {
-        let private_key: NearPrivateKey = ENCODED_PRIVATE_KEY.parse().unwrap();
+        let private_key: NearPrivateKey = PRIVATE_KEY.parse().unwrap();
 
-        assert_eq!(private_key.to_string(), ENCODED_PRIVATE_KEY);
-        assert_eq!(private_key.get_public_key().to_string(), ENCODED_PUBLIC_KEY);
+        assert_eq!(private_key.to_string(), PRIVATE_KEY);
+        assert_eq!(private_key.get_public_key().to_string(), PUBLIC_KEY);
 
-        let public_key: NearPublicKey = ENCODED_PUBLIC_KEY.parse().unwrap();
+        let public_key: NearPublicKey = PUBLIC_KEY.parse().unwrap();
 
-        assert_eq!(public_key.to_string(), ENCODED_PUBLIC_KEY);
+        assert_eq!(public_key.to_string(), PUBLIC_KEY);
     }
 }
